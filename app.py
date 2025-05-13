3b4a551d2c5736a632ae61c13b1fa5baa0e5624e
yuriiup <kaktus20110@yandex.com>
2025-05-14 01:43:14 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from requests import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mars1():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/choice/<username>')
+def two_params(username):
+    return render_template('choice.html', username=username)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
